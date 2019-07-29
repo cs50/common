c2abf9aea5dfbd81796e900bea1d89ddfe9dd123
Kareem Zidane <kzidane@cs50.harvard.edu>
2019-07-29 11:22:21 -0400

    removed FLASK_* env vars

--- etc/profile.d/baseimage.sh
+++ etc/profile.d/baseimage.sh
@@ -21,11 +21,6 @@ export LANG="C.UTF-8"
 export LC_ALL="C.UTF-8"
 export LC_CTYPE="C.UTF-8"
 
-# Flask
-export FLASK_APP="application.py"
-export FLASK_DEBUG="0"
-export FLASK_ENV="development"
-
 # Java
 export CLASSPATH=".:/usr/share/java/cs50.jar"
 export JAVA_HOME="/opt/jdk-12.0.1"
